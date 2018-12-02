84ca34d60a349ebe6964c022bb9ca2b15ee203ba
Lamour Ameur <a.lamour@live.com>
2018-12-02 09:26:54 +0100

    My project setup has successfully done !!

---
+++ backendannonce/src/main/java/com/pearl/backendannonce/App.java
@@ -0,0 +1,13 @@
+package com.pearl.backendannonce;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
